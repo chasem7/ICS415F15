e787ddc343ebfb139b5b2143690dec0027c805d1
chasem7 <cmpukahi@gmail.com>
2015-09-14 10:09:26 -1000

    Week 4 Exercise 1 upload

---
+++ IC/w4c1/scripts.js
@@ -0,0 +1,25 @@
+function clickChange(){
+
+    document.getElementById("first").style.color = "red";
+
+}
+
+
+function changeFirst(){
+
+    document.getElementById("first").style.color = "orange";
+
+}
+
+function onEnter(){
+
+    document.getElementById("second").style.color = "blue";
+    document.getElementById("second").style.font = "italic bold 24px arial,serif";
+
+}
+
+function onLeave(){
+
+    document.getElementById("third").style.color = "green";
+
+}
